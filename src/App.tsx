78385bba6b9894e9fea945b5c4411b6026d3bc15
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-05 19:26:28 +0300

    Install SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-const App = () => {
-  return <div>App</div>;
+import React from "react";
+import "./App.scss";
+
+const App: React.FC = () => {
+  return <div>Pokemon App</div>;
 };
 
 export default App;
