14c301c87c5605a759fbe1d98094d270ebaf9b98
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-06 10:28:34 +0300

    Change Name From index.scss To main.scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./store";
-import "./index.scss";
+import "./main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
