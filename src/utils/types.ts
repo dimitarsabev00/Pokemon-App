b19baf70b375db175fc40e860b6752cf5eac5e6a
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-06 12:46:21 +0300

    Fix Wrong With Null Value & Added generatedPokemonType, pokemonTypeInterface Interfaces In type.ts File

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,9 +3,26 @@ export interface GeneralSliceInitialState {
 }
 
 export interface PokemonSliceInitialState {
-  allPokemons: null | Pokemon[];
+  allPokemons: undefined | genericPokemonType[];
+  randomPokemons: undefined | generatedPokemonType[];
 }
-export interface Pokemon {
+export interface genericPokemonType {
   name: string;
   url: string;
+}
+
+export interface generatedPokemonType {
+  name: string;
+  id: number;
+  image: string;
+  types: pokemonTypeInterface[];
+}
+export interface pokemonTypeInterface {
+  [key: string]: {
+    image: string;
+    resistance: string[];
+    strength: string[];
+    weakness: string[];
+    vulnerable: string[];
+  };
 }
