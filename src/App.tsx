11ae3852c0d369db8037cff5eb7735d50d19b6f5
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-06 10:45:32 +0300

    Added Lazy Loading For All Screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Background, Footer, NavBar, Wrapper } from "./components";
 import "./App.scss";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+
+const Search = lazy(() => import("./screens/Search"));
+const MyList = lazy(() => import("./screens/MyList"));
+const About = lazy(() => import("./screens/About"));
+const Compare = lazy(() => import("./screens/Compare"));
+const Pokemon = lazy(() => import("./screens/Pokemon"));
 
 const App: React.FC = () => {
   return (
     <div className="main-container">
       <Background />
       <Router>
-        <div className="app">
-          <NavBar />
-          <Wrapper />
-          <Routes>
-            <Route element={<Search />} path="/search" />
-            <Route element={<MyList />} path="/list" />
-            <Route element={<About />} path="/about" />
-            <Route element={<Compare />} path="/compare" />
-            <Route element={<Pokemon />} path="/pokemon/:id" />
-            <Route element={<Navigate to="/pokemon/1" />} path="*" />
-          </Routes>
-          <Footer />
-        </div>
+        <Suspense fallback={<>Loading...</>}>
+          <div className="app">
+            <NavBar />
+            <Wrapper />
+            <Routes>
+              <Route element={<Search />} path="/search" />
+              <Route element={<MyList />} path="/list" />
+              <Route element={<About />} path="/about" />
+              <Route element={<Compare />} path="/compare" />
+              <Route element={<Pokemon />} path="/pokemon/:id" />
+              <Route element={<Navigate to="/pokemon/1" />} path="*" />
+            </Routes>
+            <Footer />
+          </div>
+        </Suspense>
       </Router>
     </div>
   );
