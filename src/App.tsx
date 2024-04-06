bb2711dc36af65540256dc4b15194c5df5bb9eb5
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-06 10:42:56 +0300

    Added Routing In App.tsx File

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { Background, Footer, NavBar, Wrapper } from "./components";
 import "./App.scss";
-import { BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div className="main-container">
       <Background />
-      <BrowserRouter>
+      <Router>
         <div className="app">
           <NavBar />
           <Wrapper />
-          {/* <Routes>
-              <Route element={<Search />} path="/search" />
-              <Route element={<MyList />} path="/list" />
-              <Route element={<About />} path="/about" />
-              <Route element={<Compare />} path="/compare" />
-              <Route element={<Pokemon />} path="/pokemon/:id" />
-              <Route element={<Navigate to="/pokemon/1" />} path="*" />
-            </Routes> */}
+          <Routes>
+            <Route element={<Search />} path="/search" />
+            <Route element={<MyList />} path="/list" />
+            <Route element={<About />} path="/about" />
+            <Route element={<Compare />} path="/compare" />
+            <Route element={<Pokemon />} path="/pokemon/:id" />
+            <Route element={<Navigate to="/pokemon/1" />} path="*" />
+          </Routes>
           <Footer />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
