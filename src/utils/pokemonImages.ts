eed9cb49e4a670d10ddee4ce859bf36bb155ff72
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-07 13:09:23 +0300

    Fix Bug With Get Correct Images In pokemonImages.ts File

--- src/utils/pokemonImages.ts
+++ src/utils/pokemonImages.ts
@@ -1,14 +1,17 @@
 const shinyImages = import.meta.glob('../assets/images/pokemons/shiny/*.png');
 const defaultImageS = import.meta.glob('../assets/images/pokemons/default/*.png');
 
-const fetchImages = (imagePaths) => {
+const fetchImages = async (imagePaths) => {
   const images = {};
-  Object.entries(imagePaths).forEach(([path, value]) => {
-    const key = path.split('/').pop().replace(/\.\w+$/, '');
-    images[key] = value;
-  });
+  await Promise.all(
+    Object.entries(imagePaths).map(async ([path, value]) => {
+      const key = path.split('/').pop().replace(/.\w+$/, '');
+      const imageModule = await value();
+      images[key] = imageModule.default;
+    })
+  );
   return images;
 };
 
-export const images = fetchImages(shinyImages);
-export const defaultImages = fetchImages(defaultImageS);
+export const images = await fetchImages(shinyImages);
+export const defaultImages = await fetchImages(defaultImageS);
